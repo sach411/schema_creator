ee94de4760525388a1cd43167d6bd8ba1a9b3d4d
sach411 <sachin.badgujar@gmail.com>
2023-04-09 06:46:51 -0400

    creating ov element but repeating. need to fix yjos

--- csvjson.py
+++ csvjson.py
@@ -11,21 +11,36 @@ def csv_json():
     with open('output.json', 'w') as jsonfile:
         json.dump(rows, jsonfile, indent=1)
 
+
 def csv_json_using_pandas():
     print(f"Invoking {csv_json_using_pandas.__name__}")
     df = pd.read_csv("input.csv")
-
     data_dict = df.to_dict(orient='records')
+    print(f'total records: {len(data_dict)}')
 
     nested_data = []
+    tag_set = set()
 
     for d in data_dict:
-        nested_dict = {}
-        nested_dict['description'] = d['description']
-        nested_dict['type'] = d['type']
-        nested_dict['sources'] = {"sourceName":d['sourceName'],
+        print(f"{d}")
+        if d['ovName'] not in tag_set:
+            print(f"{d['ovName']} NOT present in tag_set : {tag_set}")
+            tag_set.add(d['ovName'])
+            nested_dict = {}
+            record = {}
+            nested_dict[d["ovName"]] = {
+                "description" : d['description'],
+                "type" : d['type'],
+                "sources" : list()
+            }
+            nested_dict[d["ovName"]]['sources'] = list()
+        else:
+            print(f"{d['ovName']} present in tag_set : {tag_set}")
+
+            #nested_dict['type'] = d['type']
+        nested_dict[d["ovName"]]['sources'].append({"sourceName":d['sourceName'],
                                   "sourceType":d['sourceType'],
-                                  "sourceAttribute":d['sourceAttribute']}
+                                  "sourceAttribute":d['sourceAttribute']})
         nested_data.append(nested_dict)
 
     json_data = json.dumps(nested_data, indent=1)
