43360dc30b6a4000397657374cf350d96eb5193b
sach411 <sachin.badgujar@gmail.com>
2023-07-09 23:11:56 -0400

    schema creation : new version : handle object type : nested properties to have respective source elements

--- schema_creator_2.py
+++ schema_creator_2.py
@@ -28,40 +28,32 @@ def csv_to_json(csv_file_path, json_file_path):
     json_data = {}
     for _, row in df.iterrows():
         ov_name = row[COLUMN_OV_NAME]
-        if ov_name not in json_data:
+        if '.' not in ov_name:
             json_data[ov_name] = {
                 COLUMN_DESCRIPTION: row[COLUMN_DESCRIPTION],
                 COLUMN_TYPE: row[COLUMN_TYPE],
-                'x-collibra': {
-                    COLUMN_PRIMARY_KEY: row[COLUMN_PRIMARY_KEY],
-                    'sources': []
-                }
+                'properties': {}
             }
-        source = {
-            COLUMN_SOURCE_NAME: row[COLUMN_SOURCE_NAME],
-            COLUMN_SOURCE_TYPE: row[COLUMN_SOURCE_TYPE],
-            COLUMN_SOURCE_ATTRIBUTE: row[COLUMN_SOURCE_ATTRIBUTE]
-        }
-        if '.' in ov_name:
-            parent_ov_name, property_name = ov_name.split('.', 1)
-            if parent_ov_name not in json_data:
-                json_data[parent_ov_name] = {
-                    COLUMN_DESCRIPTION: row[COLUMN_DESCRIPTION],
-                    COLUMN_TYPE: "object",
+        else:
+            parent_name, property_name = ov_name.split('.', 1)
+            if parent_name not in json_data:
+                json_data[parent_name] = {
+                    COLUMN_DESCRIPTION: '',
+                    COLUMN_TYPE: 'object',
                     'properties': {}
                 }
-            if 'properties' not in json_data[parent_ov_name]:
-                json_data[parent_ov_name]['properties'] = {}
-            json_data[parent_ov_name]['properties'][property_name] = {
+            json_data[parent_name]['properties'][property_name] = {
                 COLUMN_DESCRIPTION: row[COLUMN_DESCRIPTION],
                 COLUMN_TYPE: row[COLUMN_TYPE],
                 'x-collibra': {
                     COLUMN_PRIMARY_KEY: row[COLUMN_PRIMARY_KEY],
-                    'sources': []
+                    'sources': [{
+                        COLUMN_SOURCE_NAME: row[COLUMN_SOURCE_NAME],
+                        COLUMN_SOURCE_TYPE: row[COLUMN_SOURCE_TYPE],
+                        COLUMN_SOURCE_ATTRIBUTE: row[COLUMN_SOURCE_ATTRIBUTE]
+                    }]
                 }
             }
-            ov_name = parent_ov_name
-        json_data[ov_name]['x-collibra']['sources'].append(source)
 
     # Write the data as JSON to a file
     with open(json_file_path, 'w') as json_file:
