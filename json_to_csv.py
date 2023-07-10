53cef4d316acaa242a21914dfc1d315b5b4c9f6d
sach411 <sachin.badgujar@gmail.com>
2023-07-10 07:24:54 -0400

    json to csv:  version 2

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import json
 
+# Define column names as constants
+COLUMN_OV_NAME = 'ovName'
+COLUMN_DESCRIPTION = 'description'
+COLUMN_TYPE = 'type'
+COLUMN_PRIMARY_KEY = 'primaryKey'
+COLUMN_SOURCE_NAME = 'sourceName'
+COLUMN_SOURCE_TYPE = 'sourceType'
+COLUMN_SOURCE_ATTRIBUTE = 'sourceAttribute'
+
 
 def json_to_csv(json_file_path, csv_file_path):
     # Read the JSON file
@@ -10,32 +19,34 @@ def json_to_csv(json_file_path, csv_file_path):
     # Convert JSON to DataFrame
     rows = []
     for ov_name, data in json_data.items():
-        row = [ov_name, data['description'], data['type'], '', '', '', '']
+        row = [ov_name, data[COLUMN_DESCRIPTION], data[COLUMN_TYPE], '', '', '', '']
         if 'x-collibra' in data:
-            row[3] = str(data['x-collibra']['primaryKey'])
+            row[3] = str(data['x-collibra'][COLUMN_PRIMARY_KEY])
             for source in data['x-collibra']['sources']:
-                row[4] = source['sourceName']
-                row[5] = source['sourceType']
-                row[6] = source['sourceAttribute']
+                row[4] = source[COLUMN_SOURCE_NAME]
+                row[5] = source[COLUMN_SOURCE_TYPE]
+                row[6] = source[COLUMN_SOURCE_ATTRIBUTE]
                 rows.append(row.copy())
         else:
             rows.append(row)
             if 'properties' in data:
                 for prop_name, prop_data in data['properties'].items():
-                    prop_row = [f"{ov_name}.{prop_name}", prop_data['description'], prop_data['type'], '', '', '', '']
+                    prop_row = [f"{ov_name}.{prop_name}", prop_data[COLUMN_DESCRIPTION], prop_data[COLUMN_TYPE], '', '',
+                                '', '']
                     if 'x-collibra' in prop_data:
-                        prop_row[3] = str(prop_data['x-collibra']['primaryKey'])
+                        prop_row[3] = str(prop_data['x-collibra'][COLUMN_PRIMARY_KEY])
                         for source in prop_data['x-collibra']['sources']:
-                            prop_row[4] = source['sourceName']
-                            prop_row[5] = source['sourceType']
-                            prop_row[6] = source['sourceAttribute']
+                            prop_row[4] = source[COLUMN_SOURCE_NAME]
+                            prop_row[5] = source[COLUMN_SOURCE_TYPE]
+                            prop_row[6] = source[COLUMN_SOURCE_ATTRIBUTE]
                             rows.append(prop_row.copy())
                     else:
                         rows.append(prop_row)
 
     # Create DataFrame
-    df = pd.DataFrame(rows, columns=['ovName', 'description', 'type', 'primaryKey', 'sourceName', 'sourceType',
-                                     'sourceAttribute'])
+    df = pd.DataFrame(rows,
+                      columns=[COLUMN_OV_NAME, COLUMN_DESCRIPTION, COLUMN_TYPE, COLUMN_PRIMARY_KEY, COLUMN_SOURCE_NAME,
+                               COLUMN_SOURCE_TYPE, COLUMN_SOURCE_ATTRIBUTE])
 
     # Write DataFrame to CSV file
     df.to_csv(csv_file_path, index=False)
