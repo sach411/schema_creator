09f65194131134f7277ff502a9dc88c5320b039f
sach411 <sachin.badgujar@gmail.com>
2023-07-13 07:43:30 -0400

    csv_to_json: array support

--- schema_creator_6.py
+++ schema_creator_6.py
@@ -15,21 +15,12 @@ def csv_to_json(csv_file_path, json_file_path):
     print(f"Input file: {csv_file_path}")
     df = pd.read_csv(csv_file_path)
 
-    # Identify duplicate rows based on selected columns
-    duplicates = df[df.duplicated(subset=[COLUMN_OV_NAME, COLUMN_SOURCE_NAME, COLUMN_SOURCE_TYPE, COLUMN_SOURCE_ATTRIBUTE], keep=False)]
-
-    # Drop duplicate rows based on selected columns
-    df.drop_duplicates(subset=[COLUMN_OV_NAME, COLUMN_SOURCE_NAME, COLUMN_SOURCE_TYPE, COLUMN_SOURCE_ATTRIBUTE], inplace=True)
-
     # Convert DataFrame to JSON structure
     json_data = {}
     for _, row in df.iterrows():
         ov_name = row[COLUMN_OV_NAME]
         description = row[COLUMN_DESCRIPTION]
         ov_type = row[COLUMN_TYPE]
-        source_name = row[COLUMN_SOURCE_NAME]
-        source_type = row[COLUMN_SOURCE_TYPE]
-        source_attribute = row[COLUMN_SOURCE_ATTRIBUTE]
 
         if '.' not in ov_name:
             if ov_name not in json_data:
@@ -37,18 +28,22 @@ def csv_to_json(csv_file_path, json_file_path):
                     'title': ov_name,
                     'description': description,
                     'type': ov_type,
-                    'x-collibra': {
-                        COLUMN_PRIMARY_KEY: row[COLUMN_PRIMARY_KEY],
-                        'sources': []
-                    }
                 }
 
-            if ov_type not in ['object', 'array']:
-                json_data[ov_name]['x-collibra']['sources'].append({
-                    COLUMN_SOURCE_NAME: source_name,
-                    COLUMN_SOURCE_TYPE: source_type,
-                    COLUMN_SOURCE_ATTRIBUTE: source_attribute
-                })
+            if ov_type == 'string' and 'x-collibra' not in json_data[ov_name]:
+                json_data[ov_name]['x-collibra'] = {
+                    'primaryKey': row[COLUMN_PRIMARY_KEY],
+                    'sources': []
+                }
+
+            if 'x-collibra' in json_data[ov_name]:
+                source_name = row[COLUMN_SOURCE_NAME]
+                if source_name not in [s['sourceName'] for s in json_data[ov_name]['x-collibra']['sources']]:
+                    json_data[ov_name]['x-collibra']['sources'].append({
+                        'sourceName': source_name,
+                        'sourceType': row[COLUMN_SOURCE_TYPE],
+                        'sourceAttribute': row[COLUMN_SOURCE_ATTRIBUTE]
+                    })
         else:
             parent_name, property_name = ov_name.split('.', 1)
             if parent_name not in json_data:
@@ -64,25 +59,27 @@ def csv_to_json(csv_file_path, json_file_path):
                 'title': property_name,
                 'description': description,
                 'type': ov_type,
-                'x-collibra': {
-                    COLUMN_PRIMARY_KEY: row[COLUMN_PRIMARY_KEY],
-                    'sources': [{
-                        COLUMN_SOURCE_NAME: source_name,
-                        COLUMN_SOURCE_TYPE: source_type,
-                        COLUMN_SOURCE_ATTRIBUTE: source_attribute
-                    }]
-                }
             }
+
+            if ov_type == 'object' and 'x-collibra' not in json_data[parent_name]['properties'][property_name]:
+                json_data[parent_name]['properties'][property_name]['x-collibra'] = {
+                    'primaryKey': row[COLUMN_PRIMARY_KEY],
+                    'sources': []
+                }
+
+            if 'x-collibra' in json_data[parent_name]['properties'][property_name]:
+                source_name = row[COLUMN_SOURCE_NAME]
+                if source_name not in [s['sourceName'] for s in json_data[parent_name]['properties'][property_name]['x-collibra']['sources']]:
+                    json_data[parent_name]['properties'][property_name]['x-collibra']['sources'].append({
+                        'sourceName': source_name,
+                        'sourceType': row[COLUMN_SOURCE_TYPE],
+                        'sourceAttribute': row[COLUMN_SOURCE_ATTRIBUTE]
+                    })
 
     # Write the data as JSON to a file
     with open(json_file_path, 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
     print(f"JSON file created successfully. {json_file_path}")
-
-    # Print duplicate rows
-    if not duplicates.empty:
-        print("Duplicate Rows:")
-        print(duplicates)
 
 # Example usage
 csv_file_path = 'input.csv'
